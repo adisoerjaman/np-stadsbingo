ababa74486a61b9f7b5ecf474ff6d723b8392af1
adisoerjaman <adisoerjaman@gmail.com>
2026-08-27 14:15:44 +0200

    prisma

--- project/prisma/migrations/20251223105359/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Assignment" ADD COLUMN     "exampleImage" TEXT;
--- project/prisma/migrations/20260606124702_add_assignment_coordinates/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Assignment" ADD COLUMN     "latitude" DOUBLE PRECISION,
-ADD COLUMN     "longitude" DOUBLE PRECISION;
